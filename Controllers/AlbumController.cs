d80fdcdda892a0b06abc6d866bb46c9e749792d7
PBeydaKurt <82771479+PBeydaKurt@users.noreply.github.com>
2022-12-31 22:10:26 +0300

    Controllerlar Eklendi

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,176 @@
+﻿using AlbumProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlbumProject.Models;
+using System;
+using System.Drawing;
+
+namespace AlbumProject.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AlbumController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Album> a = _context.Albums.Include(aa => aa.Sing).ToList();
+
+            var assay = (from aa in _context.Albums where aa.Sing != null select aa).ToList();
+            return View(assay);
+        }
+        //public IActionResult Create()
+        //{
+
+        //    return View();
+        //}
+        [HttpPost]
+        public IActionResult Create([Bind("AlbumId,AlbumTitle,AlbumSongCount,AlbumGenre,AlbumScore,AlbumYear,SingerId")] Album assay)
+        {
+            _context.Add(assay);
+            _context.SaveChanges();
+            ViewData["SingerId"] = new SelectList(_context.Singers, "SingerId", "SingerName", assay.SingerId); 
+            return RedirectToAction("Index");
+        }
+        // GET: Kitap/Create
+        public IActionResult Create()
+        {
+            ViewData["SingerId"] = new SelectList(_context.Singers, "SingerId", "SingerName");
+            return View();
+        }
+
+        // POST: Kitap/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("AssayId,AssayTitle,SubjectId")] Assay assay)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(assay);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "SubjectName", assay.SubjectId);
+        //    return View(assay);
+        //}
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var assay = await _context.Albums
+                .FirstOrDefaultAsync(m => m.AlbumId == id);
+            if (assay == null)
+            {
+                return NotFound();
+            }
+
+            return View(assay);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var assay = await _context.Albums.FindAsync(id);
+            if (assay == null)
+            {
+                return NotFound();
+            }
+            return View(assay);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SingerId,SingerName,SingerLName,SingerNationality,SingerAge")] Album assay)
+        {
+            if (id != assay.AlbumId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assay);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(assay.AlbumId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assay);
+        }
+        //**************
+
+
+        // GET: Subject1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Albums == null)
+            {
+                return NotFound();
+            }
+
+            var assay = await _context.Albums
+                .FirstOrDefaultAsync(m => m.AlbumId == id);
+            if (assay == null)
+            {
+                return NotFound();
+            }
+
+            return View(assay);
+        }
+
+        // POST: Subject1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Albums == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Assays'  is null.");
+            }
+            var assay = await _context.Albums.FindAsync(id);
+            if (assay != null)
+            {
+                _context.Albums.Remove(assay);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _context.Albums.Any(e => e.AlbumId == id);
+        }
+        //public IActionResult Index()
+        //{
+        //    List<Assay> a = _context.Assays.Include(aa => aa.Subj).ToList();
+
+        //    return View(a);
+        //}
+        //
+        ///lütfen bu da düzgün çalıssin 
+    }
+}
--- Controllers/SingerController.cs
+++ Controllers/SingerController.cs
@@ -1,6 +1,8 @@
-﻿using AlbumProject.Data;
+﻿/*using AlbumProject.Data;
 using Microsoft.AspNetCore.Mvc;
 using AlbumProject.Models;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace AlbumProject.Controllers
 {
@@ -23,7 +25,7 @@ namespace AlbumProject.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create([Bind("SingerName","SingerLName")]Singer a)
+        public IActionResult Create([Bind("SingerName,SingerLName,SingerNationality,SingerAge")]Singer a)
         {
             _context.Add(a);
             _context.SaveChanges();
@@ -31,3 +33,289 @@ namespace AlbumProject.Controllers
         }
     }
 }
+
+*/
+/*
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AlbumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlbumProject.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AlbumProject.Controllers
+    
+{
+    public class SingerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SingerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        static List<Singer> singers = new List<Singer>();
+        // GET: OgrenciController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View(singers);
+        }
+        // GET: OgrenciController/Details/5
+        public ActionResult Details(int id)
+        {
+            Singer gelen = new Singer();
+            // gelen = ogrenciler.Find(x => x.OgrNo = id);
+            foreach (var sngr in singers)
+            {
+                if (Convert.ToInt32(sngr.SingerId) == id)
+                {
+                    // ogr.OgrAd=gelen.
+                    gelen = sngr;
+                    break;
+                }
+
+            }
+            if (gelen.SingerName is null)
+                return View("Hata");
+            return View(gelen);
+        }
+
+        // GET: OgrenciController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OgrenciController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Singer sngr)
+        {
+            if (ModelState.IsValid)
+            {
+                singers.Add(sngr);
+                TempData["sngr"] = sngr.SingerName + " Adlı şarkıcı Eklendi";
+                return RedirectToAction("List");
+                _context.Add(sngr);
+                _context.SaveChanges();
+               
+            }
+            else
+            {
+                return View("Hata");
+            }
+        }
+
+        // GET: OgrenciController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OgrenciController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OgrenciController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Singer silinecek = new Singer();
+            foreach (var sngr in singers)
+            {
+                if (Convert.ToInt32(sngr.SingerId) == id)
+                {
+                    silinecek = sngr;
+                    break;
+                }
+            }
+            if (silinecek.SingerName is null)
+            {
+                return View("Hata");
+            }
+            else
+            {
+                TempData["ogr"] = silinecek.SingerName + " adlı öğrenci silindi";
+                singers.Remove(silinecek);
+                return RedirectToAction("List");
+            }
+
+
+
+        }
+
+        // POST: OgrenciController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+*/
+
+using AlbumProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AlbumProject.Models;
+
+namespace AlbumProject.Controllers
+{
+    public class SingerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SingerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Singer> a = _context.Singers.ToList();
+            return View(a);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind("SingerName,SingerLName,SingerNationality,SingerAge")] Singer a)
+        {
+            _context.Add(a);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Singers == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Singers
+                .FirstOrDefaultAsync(m => m.SingerId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Singers == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Singers.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return View(subject);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SingerId,SingerName,SingerLName,SingerNationality,SingerAge")] Singer subject)
+        {
+            if (id != subject.SingerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.SingerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subject);
+        }
+        //**************
+
+
+        // GET: Subject1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Singers == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Singers
+                .FirstOrDefaultAsync(m => m.SingerId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Subject1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Singers == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Subjects'  is null.");
+            }
+            var subject = await _context.Singers.FindAsync(id);
+            if (subject != null)
+            {
+                _context.Singers.Remove(subject);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Singers.Any(e => e.SingerId == id);
+        }
+    }
+}
+
