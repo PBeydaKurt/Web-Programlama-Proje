4709d89138a402f8e6bdb7fb954d5490241a8e5d
PBeydaKurt <82771479+PBeydaKurt@users.noreply.github.com>
2023-01-01 23:04:01 +0300

    Son Eklemeler

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 using AlbumProject.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AlbumProject.Models;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.CodeAnalysis.Host;
+using System.Globalization;
+using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +19,70 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity< UserDetails, IdentityRole > ()
+    .AddDefaultTokenProviders()
+    .AddDefaultUI()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+   
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<LanguageService>();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddMvc()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization(options =>
+    {
+        options.DataAnnotationLocalizerProvider = (type, factory) =>
+        {
+
+            var assemblyName = new AssemblyName(typeof(ShareResource).GetTypeInfo().Assembly.FullName);
+
+            return factory.Create("ShareResource", assemblyName.Name);
+
+        };
+
+    });
+
+
+
+builder.Services.Configure<RequestLocalizationOptions>(
+    options =>
+    {
+        var supportedCultures = new List<CultureInfo>
+            {
+                            new CultureInfo("en-US"),
+                            new CultureInfo("es-ES"),
+                            new CultureInfo("nn-NO"),
+            };
+
+
+
+        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+
+        options.SupportedCultures = supportedCultures;
+        options.SupportedUICultures = supportedCultures;
+        options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+
+    });
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    
+
+});
+
+
+builder.Services.AddAuthentication(
+        CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate();
+
 
 var app = builder.Build();
 
@@ -27,6 +97,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+var options = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(options.Value);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
